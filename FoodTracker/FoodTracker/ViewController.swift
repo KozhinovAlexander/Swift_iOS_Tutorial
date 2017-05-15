c0a42d4dd7ff0fde9d6464af191e55dea1a06f49
Nukersson <AlexanderKozhinov@yandex.com>
2017-05-15 18:17:37 +0200

    Images were added and more

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +44,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
+    }
+
+    // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Recognizes taps from photoImageView
     }
 }
 
