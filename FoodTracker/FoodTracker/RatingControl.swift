1d7f07337cb33670c01d7edd731458b4b1edfbf5
Alexander Kozhinov <AlexanderKozhinov@yandex.com>
2017-05-22 10:44:49 +0200

    ...

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by nuke on 22.05.17.
+//  Copyright © 2017 Nukersson. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
